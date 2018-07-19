4dc2b6057c03928aadf903ca26efdabcbaa1c855
Katastroffa <41030462+Katastroffa@users.noreply.github.com>
2018-07-19 14:29:35 -0700

    Fix functionsumm (#1)
    * added .new.cosole.log
    
    * added .new.line.log
    
    * added.emty.log
    
    * added.sum.function
    
    * fix summ function

--- first project.js
+++ first project.js
@@ -1,3 +1,11 @@
 console.log('hello world');
 
 console.log('---');
+
+console.log('emty');
+
+function sum(a,b) {
+    if (typeof a !== 'number') return 0;
+    return a + b;
+}
+console.log(sum());
